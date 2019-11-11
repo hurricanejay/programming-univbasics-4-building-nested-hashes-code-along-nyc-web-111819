c382be10058314ec01e09bf606f3f96d6063d64b
Jay Wen <jaywen89@gmail.com>
2019-11-11 20:04:28 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -24,8 +24,8 @@ def bonus
      :hero => {name: "Romeo", age: "15", status: "alive"},
     :heroine => {name: "Juliet", age: "15", status: "alive", "dead"},
     
-    hero[:status]
-    heroine[:status]
+    hero[:status][2]
+    heroine[:status][2]
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
