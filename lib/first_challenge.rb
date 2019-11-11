5e9d0b320ec86a8ca2b303b47860bc5c8153f11c
Jay Wen <jaywen89@gmail.com>
2019-11-11 19:31:56 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,9 +1,9 @@
 def first_challenge
 epic_tragedy = {
    :montague => {
-      :patriarch => {:name = "Lord Montague"},
-      :matriarch => {:age = "54"},
-      :hero => {:name = "Romeo", :age },
+      :patriarch => {:name => "Lord Montague"},
+      :matriarch => {:age => "54"},
+      :hero => {:name => "Romeo", :age => "15", :status => "Alive" },
       :hero_friends => []
    },
    :capulet => {
